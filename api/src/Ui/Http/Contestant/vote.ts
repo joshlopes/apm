5c5455580d9e68338a898d878753b57a1c0b308a
Jose Lopes <josluis.lopes@gmail.com>
2024-04-07 12:12:09 +0100

    chore: tidy

--- api/src/Ui/Http/Contestant/vote.ts
+++ api/src/Ui/Http/Contestant/vote.ts
@@ -3,8 +3,6 @@ import {handleCommand} from "../handleCommandUtil";
 import VoteContestantCommand from "../../../Application/Write/Contestant/VoteContestant/VoteContestantCommand";
 import VoteId from "../../../Domain/Contestant/VoteId";
 import ContestantId from "../../../Domain/Contestant/ContestantId";
-import GetContestantCommand from "../../../Application/Query/Contestant/GetContestant/GetContestantCommand";
-import Contestant from "../../../Domain/Contestant/Contestant";
 
 export const vote: RequestHandler = async (req: Request, resp: Response) => {
     const voteId = VoteId.create()
