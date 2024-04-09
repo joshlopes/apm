ebc7629c66f4d360e083bdd7578212b6f52dc531
Jose Lopes <josluis.lopes@gmail.com>
2024-04-09 17:11:46 +0100

    chore: add announcement component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,10 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import ContestOverview from "./components/Contest/ContestOverview";
-import {Link} from "@mui/material";
+import {Grid, Link} from "@mui/material";
 import Feed from "./components/Contest/Feed";
 import ButtonAppBar from "./StickBar";
+import Announcement from "./components/Share/Announcement";
 
 function Copyright() {
     return (
@@ -28,12 +29,17 @@ export default function App() {
                     <ButtonAppBar />
                     <Container>
                         <Box sx={{ my: 4 }}>
-                                <Routes>
-                                    <Route index element={<Feed />} />
-                                    <Route path="/category" element={<Feed />} />
-                                    <Route path="/category/:category" element={<Feed />} />
-                                    <Route path="/show/:id" element={<ContestOverview />} />
-                                </Routes>
+                            <Grid container spacing={3}>
+                                <Grid item xs={12}>
+                                    <Announcement message='Parem de scammar, please 😠️'/>
+                                </Grid>
+                            </Grid>
+                            <Routes>
+                                <Route index element={<Feed />} />
+                                <Route path="/category" element={<Feed />} />
+                                <Route path="/category/:category" element={<Feed />} />
+                                <Route path="/show/:id" element={<ContestOverview />} />
+                            </Routes>
                         </Box>
                     </Container>
                 </Box>
---
+++ frontend/src/components/Share/Announcement.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AnnouncementIcon from '@mui/icons-material/Announcement';
+
+interface AnnouncementProps {
+    message: string;
+}
+
+const Announcement: React.FC<AnnouncementProps> = ({ message }) => {
+    return (
+        <Accordion sx={{ bgcolor: 'info.main', color: 'common.white', boxSizing: 'border-box'}}>
+            <AccordionSummary
+                expandIcon={<ExpandMoreIcon sx={{color: 'common.white'}}/>}
+            >
+                <Typography>
+                    <AnnouncementIcon fontSize={"small"} />
+                    &nbsp;Anúncio Importante
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    {message}
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+    );
+}
+
+export default Announcement;
